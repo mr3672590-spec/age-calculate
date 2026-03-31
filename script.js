670dbfb678c1238a3d21899d2ec0b6707c302551
sohel rana <mr367290@gmail.com>
2026-03-31 10:42:57 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+let userInput = document.getElementById("date");
+let button = document.querySelector("button");
+
+let result = document.createElement("p");
+document.querySelector(".calculator").appendChild(result);
+
+button.addEventListener("click", () => {
+    let birthDate = new Date(userInput.value);
+
+    if (!userInput.value) {
+        result.innerHTML = "Please select your birth date!";
+        return;
+    }
+
+    let today = new Date();
+
+    let d1 = birthDate.getDate();
+    let m1 = birthDate.getMonth() + 1;
+    let y1 = birthDate.getFullYear();
+
+    let d2 = today.getDate();
+    let m2 = today.getMonth() + 1;
+    let y2 = today.getFullYear();
+
+    let d3, m3, y3;
+
+    y3 = y2 - y1;
+
+    if (m2 >= m1) {
+        m3 = m2 - m1;
+    } else {
+        y3--;
+        m3 = 12 + m2 - m1;
+    }
+
+    if (d2 >= d1) {
+        d3 = d2 - d1;
+    } else {
+        m3--;
+        d3 = getDaysInMonth(y2, m2 - 1) + d2 - d1;
+    }
+
+    if (m3 < 0) {
+        m3 = 11;
+        y3--;
+    }
+
+    result.innerHTML = `Your Age is <span>${y3}</span> Years, <span>${m3}</span> Months and <span>${d3}</span> Days`;
+});
+
+
+function getDaysInMonth(year, month) {
+    return new Date(year, month, 0).getDate();
+}
